545683728eb63fd9a1aa8bf9327dfdb63b98b036
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2025-04-27 00:06:59 +0000

    docs: add resend email token docs

--- src/models/tokens/use-cases/resend-email/resend-email.controller.ts
+++ src/models/tokens/use-cases/resend-email/resend-email.controller.ts
@@ -6,19 +6,35 @@ import {
   Post,
   UseInterceptors,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { DatabaseTransactionInterceptor } from '@/interceptors/database-transaction.interceptor';
 import { ResendEmailRequestDto } from './resend-email.request.dto';
 import { ResendEmailService } from './resend-email.service';
 import { Payload } from '@/decorators/payload.decorator';
 
-@Controller()
+@ApiTags('Tokens')
+@ApiBearerAuth()
+@Controller('tokens')
 @UseInterceptors(DatabaseTransactionInterceptor)
 export class ResendEmailController {
   constructor(private readonly resendEmailService: ResendEmailService) {}
 
+  @ApiOperation({ summary: 'Resend token email' })
+  @ApiOkResponse({
+    description: 'Email resent successfully',
+    example: {
+      statusCode: 200,
+      message: 'Email resent successfully',
+    },
+  })
   @HttpCode(HttpStatus.OK)
-  @Post('tokens/resend-email')
+  @Post('resend-email')
   async handle(
     @Payload('sub') userId: string,
     @Body() { tokenType }: ResendEmailRequestDto,
